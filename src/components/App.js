7144fb9adf33275e276c57561136abbf38568328
Oliwia Gierczak <oliwia.giercza@gmail.com>
2023-02-24 12:58:47 +0100

    adding pagination

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./../styles.css/App.css";
+import CreateLink from "./CreateLink";
+import LinkList from "./LinkList";
+import Header from "./Header";
+import Login from "./Login";
+import Search from "./Search";
+
+const App = () => (
+  <div className="center w85">
+    <Header />
+    <div className="ph3 pv1 background-gray">
+    <Routes>
+        <Route
+          path="/"
+          element={<Navigate replace to="/new/1" />}
+          />
+        <Route
+          path="/"
+          element={<Navigate replace to="/new" />}
+        />
+
+        <Route
+          path="/create"
+          element={<CreateLink/>}
+        />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/search"element={<Search/>}/>
+         <Route path="/top" element={<LinkList/>} />
+        <Route
+          path="/new/:page"
+          element={<LinkList/>}
+        />
+      </Routes>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/components/LinkList.js
@@ -0,0 +1,115 @@
+import React from "react";
+import Link from "./Link";
+import { useQuery, gql } from "@apollo/client";
+import { useLocation } from "react-router-dom";
+
+const getQueryVariables = (isNewPage, page) => {
+  const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+  const take = isNewPage ? LINKS_PER_PAGE : 100;
+  const orderBy = { createdAt: 'desc' };
+  return { take, skip, orderBy };
+};
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export const FEED_QUERY = gql`
+query FeedQuery(
+  $take: Int
+  $skip: Int
+  $orderBy: LinkOrderByInput
+) {
+  feed(take: $take, skip: $skip, orderBy: $orderBy) {
+    id
+    links {
+      id
+      createdAt
+      url
+      description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+    count
+  }
+}
+`;
+
+const LinkList = () => {
+  const location = useLocation();
+  const isNewPage = location.pathname.includes(
+    'new'
+  );
+  const pageIndexParams = location.pathname.split(
+    '/'
+  );
+  const page = parseInt(
+    pageIndexParams[pageIndexParams.length - 1]
+  );
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE : 0;
+  const {
+    data,
+    loading,
+    error,
+    subscribeToMore
+  } = useQuery(FEED_QUERY, {
+    variables: getQueryVariables(isNewPage, page),
+  });
+
+  subscribeToMore({ document: NEW_LINKS_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(
+        ({ id }) => id === newLink.id
+      );
+      if (exists) return prev;
+  
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      });
+    }});
+
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LinkList;
